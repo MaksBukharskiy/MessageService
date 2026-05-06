55243641512d43b2df160841b353e8cbe644b00a
freedom_lovver <maksbuharskij16@gmail.com>
2026-05-07 00:20:00 +0500

    Refactor Kafka consumer to handle OrderCreateEvent with retry logic using RetryableTopic and basic logging for incoming messages processing.

--- src/main/java/org/messgage/messageservice/config/consumer/KafkaOrderConsumer.java
+++ src/main/java/org/messgage/messageservice/config/consumer/KafkaOrderConsumer.java
@@ -2,15 +2,34 @@ package org.messgage.messageservice.config.consumer;
 
 import lombok.extern.slf4j.Slf4j;
 import org.messgage.messageservice.orders.OrderCreateEvent;
+import org.springframework.kafka.annotation.BackOff;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.annotation.RetryableTopic;
 import org.springframework.stereotype.Component;
 
 @Component
 @Slf4j
 public class KafkaOrderConsumer {
 
+    @RetryableTopic(
+            attempts = "3",
+            backOff = @BackOff(delay = 2000),
+            dltTopicSuffix = ".DLT"
+    )
+
     @KafkaListener(topics = "orders", groupId = "order-group")
     public void listen(OrderCreateEvent event){
-        log.info("\n\n🔥 Received from Kafka: {}", event);
+        log.info("🔥 Received from Kafka: {}", event);
+
+        if ("fail".equals(event.product())) {
+            throw new RuntimeException("Test error");
+        }
     }
+
+    @KafkaListener(topics = "orders.DLT", groupId = "order-group")
+    public void listenDeadMessages(OrderCreateEvent event){
+        log.info("💀 DLT message received: {}", event);
+    }
+
+
 }
